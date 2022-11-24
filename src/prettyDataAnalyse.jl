5655ae2306f8dcc2895b3358cf4c91400c96e210
Nicolas Descouens <nicolas.descouens@exotec.com>
2022-11-24 09:09:40 +0000

    rounding data table

--- src/prettyDataAnalyse.jl
+++ src/prettyDataAnalyse.jl
@@ -4,10 +4,11 @@ export prettyTable, prettyPlot
 
 function computeTable(name, performances, threshhold)
     n = length(performances)
+    performances = round.(performances)
     pass_fail = [x > threshhold ? "PASS" : "FAIL" for x in performances];
     pass_fail = push!(pass_fail, sum(performances) > n * threshhold ? "PASS" : "FAIL");
     push!(name, "All ");
-    performances = append!(performances,sum(performances))
+    performances = append!(performances,floor(sum(performances)))
     estimated = vcat([threshhold for i in 1:n], [n * threshhold])
 
     return performances, estimated, pass_fail
@@ -25,7 +26,9 @@ end;
 
 function prettyPlot(midWindow, df, interp_lin, mean, begin_shift, end_shift, resolution=10)
     df = filter(:datedone => r -> r > begin_shift * 60 && r < df.datedone[end] - end_shift * 60, df)
+    println(df.datedone[end-0])
     common_time = df.datedone[1+midWindow]:resolution:df.datedone[end-midWindow]
+    println(common_time)
 
     return plot(common_time / 60, [sum(map(i -> i.(common_time), interp_lin)) [mean for i in 1:length(common_time)]], label="", xlabel="Time (min)", ylabel="Perf. (bin/h)")
 end
